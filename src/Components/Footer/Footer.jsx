44b41ab3da43748105953319dd15ef19af4a3ea1
alamnahid <nahidalam13765@gmail.com>
2024-01-05 12:29:26 +0600

    implement footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,9 +6,32 @@ import { FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-       <div>
-        
-       </div>
+        <div className="h-[30rem] footer flex flex-col justify-center pl-[15%]">
+            <div className="relative w-[30vw]">
+                <input className="w-[30vw] h-[3.8rem] rounded-2xl shadow-sm flex pl-10 placeholder:text-gray-400 outline-none border-none pr-4" type="search" name="search" id="" placeholder="Enter Your email" />
+
+                <button className="bg-[#fc6011] absolute top-[0.4rem] right-2 text-white w-[8vw] h-[3rem] rounded-2xl flex justify-center items-center gap-1">Subscribe <IoIosArrowRoundForward className="text-2xl" /></button>
+
+            </div>
+
+            <h1 className="mt-20 text-4xl font-bold">pti.</h1>
+
+            <div className="flex items-center gap-[20%]">
+                <p className="text-xl font-semibold">Copyright&#169;Tripp.All Right Reserved</p>
+                <div className="flex gap-3">
+                    <div className="w-[5rem] h-[5rem] rounded-[5rem] bg-gray-300 flex justify-center items-center text-4xl text-orange-800">
+                        <FaGoogle/>
+                    </div>
+                    <div className="w-[5rem] h-[5rem] rounded-[5rem] bg-gray-300 flex justify-center items-center text-4xl text-orange-800">
+                        <FaTwitter/>
+                    </div>
+                    <div className="w-[5rem] h-[5rem] rounded-[5rem] bg-gray-300 flex justify-center items-center text-4xl text-orange-800">
+                        <FaInstagram/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 };
 
